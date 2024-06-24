83b33251e76c1d1f3413bd94e39012bef732f63f
public_hysteria <juuzodes@yandex.ru>
2024-06-24 21:58:07 +0300

    some fts

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-const express = require('express');
-const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
-const cookieParser = require('cookie-parser');
-const userRouter = require('./routers/userRouter.js')
-const profileRouter = require('./routers/profileRouter.js')
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
 
-app.use(express.json());
-//app.use(cookieParser)
-app.get('/',(req,res)=>{return res.send('hi')})
-app.use('/user', userRouter)
-app.use('/profile', profileRouter)
-
-app.listen(PORT, () => {
-    console.log(process.env.TOKEN_SECRET)
-    console.log(`Server is running on port ${PORT}`);
+const PROTO_PATH = __dirname + '/user.proto';
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    arrays: true
 });
+const userProto = grpc.loadPackageDefinition(packageDefinition).user;
+
+const server = new grpc.Server()
+const users = [{"id":"1","password":"12345", "email":"ivan@a.ru"},{"id":"1","password":"12345", "email":"ivan@a.ru"},{"id":"1","password":"12345", "email":"ivan@a.ru"}]
+server.addService(userProto.UserService.service,{
+    getAll:(_,callback)=>{
+        console.log(users)
+        callback(null,{users})
+    },
+    get:(call, callback)=>{
+        console.log(call.request.id)
+        const user = users.find(n=>n.id==call.request.id)
+        console.log(user)
+        if(!user) callback({
+            code: grpc.status.NOT_FOUND,
+            details: "Not found"
+        });
+        callback(null, user)
+        
+    }
+})
+server.bindAsync('0.0.0.0:3000', grpc.ServerCredentials.createInsecure(),(err)=>{
+    if (err) 
+        console.error("Failed to bind: ", err);
+      
+})
